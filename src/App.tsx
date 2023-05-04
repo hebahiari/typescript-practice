5a32ec0122a91e52c4f59cb6ac729f4385c0e03d
Heba <hebahiary+github@gmail.com>
2023-05-04 07:49:33 -0700

    initial upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { ReactElement, ReactNode } from 'react';
+
 import './App.css';
+
+//conventional props
+
+function Heading({title}: {title: string;}) {
+  return (
+    <h1>{title}</h1>
+  )
+}
+
+function HeadingWithContent({children}: {children: ReactNode}): ReactElement {
+  return (
+    <h1>{children}</h1>
+  )
+}
+
+//Default Props
+const defaultContainerProps = {
+  heading: <strong>My Heading</strong>
+}
+function Container({heading, children}: {children: ReactNode} & typeof defaultContainerProps): ReactElement {
+  return (
+    <div>
+      <h1>{children}</h1>
+      <h1>{heading}</h1>
+    </div>
+    
+  )
+}
+Container.defaultProps = defaultContainerProps;
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Heading title={"Hello There"} />
+     <HeadingWithContent><strong>Hey</strong></HeadingWithContent>
+     <Container>Foo</Container>
     </div>
   );
 }
