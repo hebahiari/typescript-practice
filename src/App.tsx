8048c85f20a09e2102e1611a84fe7c30a3aaa766
Heba <hebahiary+github@gmail.com>
2023-10-04 14:51:28 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement, ReactNode } from 'react';
 
 import './App.css';
+import { stat } from 'fs';
 
 //conventional props
 
@@ -32,12 +33,34 @@ function Container({heading, children}: {children: ReactNode} & typeof defaultCo
 Container.defaultProps = defaultContainerProps;
 
 
+//functional props
+function TextWithNumber({
+  header,
+  children
+}: {
+  header: (num: number) => ReactNode
+  children: (num: number) => ReactNode
+
+}) {
+  const [state, stateSet] = React.useState<number>(1)
+
+  return (
+    <div>
+      <h2>{header(state)}</h2>
+      <div>{children(state)}</div><div>
+        <button onClick={()=> stateSet(state + 1)}>Add</button>
+      </div>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
      <Heading title={"Hello There"} />
      <HeadingWithContent><strong>Hey</strong></HeadingWithContent>
      <Container>Foo</Container>
+     <TextWithNumber header={(num: number) => <span>Header is </span>}>{(num: number) => <div>Todays number is {num}</div>}</TextWithNumber>
     </div>
   );
 }
